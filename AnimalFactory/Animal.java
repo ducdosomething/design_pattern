7322f2466120b3c61daf019c8d5ba23a6ebe06c8
ducdosomething <duclehoang211201@gmail.com>
2024-04-02 08:41:52 +0700

    Animal factory

---
+++ AnimalFactory/Animal.java
@@ -0,0 +1,5 @@
+package AnimalFactory;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ AnimalFactory/Dog.java
@@ -0,0 +1,8 @@
+package AnimalFactory;
+
+public class Dog extends Animal {
+    @Override
+    public String makeSound() {
+        return "Woof";
+    }
+}
